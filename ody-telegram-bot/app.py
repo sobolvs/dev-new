730ff23eea7c81f13ea914dc37f2bb82ece727f5
sobolvs <sobolev.service@gmail.com>
2023-11-27 09:27:02 +0300

    added credentials

--- ody-telegram-bot/app.py
+++ ody-telegram-bot/app.py
@@ -1,1 +1,8 @@
 from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import config
+
+TOKEN: Final = config.tg_token
+BOT_USERNAME: Final = config.tg_username
+
