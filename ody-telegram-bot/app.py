eb8ee18c2ac3d08ee9f7e10698127702580a2461
sobolvs <sobolev.service@gmail.com>
2023-11-27 09:35:11 +0300

    addedd first commands

--- ody-telegram-bot/app.py
+++ ody-telegram-bot/app.py
@@ -6,3 +6,12 @@ import config
 TOKEN: Final = config.tg_token
 BOT_USERNAME: Final = config.tg_username
 
+# Commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hello! Thanks for chatting with me!')
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('I am dev bot! Please type your question')
+
+async def send_task_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('I am dev bot! Please send your task')
